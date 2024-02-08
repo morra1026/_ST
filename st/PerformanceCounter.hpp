428e0061ec523daea76af98c2dc1fc7b4cb1b145
SeungTae Kim <morra1026@live.com>
2024-02-08 13:01:39 +0900

    함수를 받아서 걸린 시간 측정하는 Measure()추가

--- st/PerformanceCounter.hpp
+++ st/PerformanceCounter.hpp
@@ -1,25 +1,61 @@
 #pragma once
 
+#include <functional>
+
 #include "st/DateTime.hpp"
 
 namespace st
 {
     class PerformanceCounter
     {
+        using TestFunc = std::function<bool(void)>;
+       
     public:
+        auto Now() const
+        {
+            return st::DateTime::Now();
+        }
+
         auto Check(const bool isTotal = false)
         {
             const auto now = st::DateTime::Now();
 
             if (isTotal)
             {
-                return std::chrono::duration_cast<std::chrono::milliseconds>(now - start_);
+                return (now - start_).ToMilliseconds();
             }
             else
             {
                 auto diff = now - prev_;
                 prev_ = now;
-                return std::chrono::duration_cast<std::chrono::milliseconds>(diff);
+                return diff.ToMilliseconds();
+            }
+        }
+
+        void Measure(TestFunc f) const
+        {
+            static constexpr st::StringView dividerOut_{_T("====================================================\n")};       
+            static constexpr st::StringView dividerIn_{_T("----------------------------------------------------\n")};
+
+            if (f)
+            {
+                auto start = Now();
+
+                tcout << dividerOut_;
+                tcout << _T("PerformanceCheck Start : ") << start << tendl;
+
+                tcout << dividerIn_;
+                auto result = f();
+                tcout << dividerIn_;
+
+                auto end = Now();
+                auto diff = end - start;
+
+                tcout << _T("PerformanceCheck Result : ") << std::boolalpha << result << std::noboolalpha << tendl;
+                tcout << _T("PerformanceCheck Duration : ") << diff.ToMilliseconds() << tendl;
+                tcout << _T("PerformanceCheck End : ") << end << tendl;
+                tcout << dividerOut_ << tendl;
+
             }
         }
 
